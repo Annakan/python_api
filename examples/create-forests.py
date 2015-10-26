820101f0937b30d417ef6e0e8b976a26a90bb0f7
XM <Annakan@users.noreply.github.com>
2015-10-26 08:37:50 +0100

    Let's remove some Java from python

--- examples/create-forests.py
+++ examples/create-forests.py
@@ -8,87 +8,26 @@ from marklogic.models.forest import Forest
 from marklogic.connection import Connection
 from marklogic.exceptions import *
 
+
 class CreateForests:
-    def __init__(self):
-        self.marklogic       = None
-        self.host            = "localhost"
-        self.hosts           = None
-        self.prefix          = "F"
-        self.forests         = 2
-        self.start           = 1
-        self.failover        = "none"
-        self.failhost        = None
-        self.adminuser       = "admin"
-        self.adminpass       = "admin"
-        self.data_dir        = None
-        self.large_data_dir  = None
-        self.fast_data_dir   = None
-        self.database        = None
-        self.dry_run         = False
-
-    # TODO: better checking of argument types
-
-    def set_rest_host(self, host):
+    def __init__(self, marklogic=None, host="localhost", hosts=None, prefix="F", forests=2, start_forest=1,
+                 failover="none", failover_host=None, adminuser="admin", adminpass="admin", data_dir=None,
+                 large_data_dir=None, fast_data_dir=None, database=None, dry_run=False):
+        self.marklogic = marklogic
         self.host = host
-        return self
-
-    def set_hosts(self, hosts):
-        if hosts is None:
-            self.hosts = None
-        else:
-            if isinstance(hosts, list):
-                self.hosts = hosts
-            else:
-                self.hosts = [ hosts ]
-        return self
-
-    def set_prefix(self, prefix):
+        self.hosts = hosts if isinstance(hosts, list) or None else [hosts]
         self.prefix = prefix
-        return self
-
-    def set_forest_count(self, forests):
-        self.forests = int(forests)
-        return self
-
-    def set_start_number(self, start):
-        self.start = start
-        return self
-
-    def set_failover(self, failover):
+        self.forests = forests
+        self.start = start_forest
         self.failover = failover
-        return self
-
-    def set_failover_host(self, failhost):
-        self.failhost = failhost
-        return self
-
-    def set_user(self, adminuser):
+        self.failhost = failover_host
         self.adminuser = adminuser
-        return self
-
-    def set_pass(self, adminpass):
         self.adminpass = adminpass
-        return self
-
-    def set_data_dir(self, data_dir):
         self.data_dir = data_dir
-        return self
-
-    def set_large_data_dir(self, large_data_dir):
         self.large_data_dir = large_data_dir
-        return self
-
-    def set_fast_data_dir(self, fast_data_dir):
         self.fast_data_dir = fast_data_dir
-        return self
-
-    def set_database(self, database):
         self.database = database
-        return self
-
-    def set_dry_run(self, dry_run):
         self.dry_run = dry_run
-        return self
 
     def create_forests(self):
         conn = Connection(self.host,
@@ -106,10 +45,8 @@ class CreateForests:
             host_names = self.hosts
 
         exists = []
-        host_index = 0
-        for host_name in host_names:
-            host_index += 1
-            for count in range(1,self.forests + 1):
+        for host_index, host_name in enumerate(host_names):
+            for count in range(1, self.forests + 1):
                 name = self.prefix + "-" + str(host_index) + "-" + str(count)
                 forest = Forest(name, host_name, conn)
                 if forest.exists():
@@ -121,10 +58,8 @@ class CreateForests:
                 print("   ", f)
             sys.exit(1)
 
-        host_index = 0
-        for host_name in host_names:
-            host_index += 1
-            for count in range(self.start,self.start + self.forests):
+        for host_index, host_name in enumerate(host_names):
+            for count in range(self.start, self.start + self.forests):
                 name = self.prefix + "-" + str(host_index) + "-" + str(count)
                 forest = Forest(name, host_name, conn)
                 if self.data_dir is not None:
@@ -149,11 +84,12 @@ class CreateForests:
 
         print("Finished")
 
+
 def main():
     parser = argparse.ArgumentParser(description="Create forests on a cluster")
 
     parser.add_argument('--rest-host', default='localhost',
-                        metavar='HOST',
+                        metavar='HOST', dest='marklogic',
                         help="The host to use for REST configuration")
     parser.add_argument('--host', nargs="*",
                         help='Hosts on which to create forests')
@@ -167,14 +103,14 @@ def main():
                         metavar='FIRST',
                         help='The forest index at which to start numbering')
     parser.add_argument('--failover', default='none',
-                        choices=['local','shared','none'],
+                        choices=['local', 'shared', 'none'],
                         help='Failover configuration')
     parser.add_argument('--failover-host',
                         metavar='HOST',
                         help='For shared-disk failover, use this failover host for all forests')
-    parser.add_argument('--user', default='admin',
+    parser.add_argument('--adminuser', default='admin',
                         help='The user for REST configuration')
-    parser.add_argument('--pass', default='admin',
+    parser.add_argument('--adminpass', default='admin',
                         help='The password for REST configuration')
     parser.add_argument('--data-dir',
                         metavar='DIR',
@@ -194,51 +130,15 @@ def main():
 
     args = vars(parser.parse_args())
 
-    creator = CreateForests()
-
-    for opt in args:
-        arg = args[opt]
-
-        if opt == 'rest_host':
-            creator.set_rest_host(arg)
-        elif opt == 'host':
-            creator.set_hosts(arg)
-        elif opt == 'prefix':
-            creator.set_prefix(arg)
-        elif opt == 'forests':
-            creator.set_forest_count(arg)
-        elif opt == 'start_forest':
-            creator.set_start_number(arg)
-        elif opt == 'failover':
-            creator.set_failover(arg)
-        elif opt == 'failover_host':
-            creator.set_failover_host(arg)
-        elif opt == 'user':
-            creator.set_user(arg)
-        elif opt == 'pass':
-            creator.set_pass(arg)
-        elif opt == 'data_dir':
-            creator.set_data_dir(arg)
-        elif opt == 'large_data_dir':
-            creator.set_large_data_dir(arg)
-        elif opt == 'fast_data_dir':
-            creator.set_fast_data_dir(arg)
-        elif opt == 'database':
-            creator.set_database(arg)
-        elif opt == 'dry_run':
-            creator.set_dry_run(arg)
-        elif opt == 'debug':
-            pass
-        else:
-            print("Error: unexpected argument: " + opt)
-            sys.exit(1)
-
     if args['debug']:
         logging.basicConfig(level=logging.WARNING)
         logging.getLogger("requests").setLevel(logging.WARNING)
         logging.getLogger("marklogic").setLevel(logging.DEBUG)
+    del args['debug']
 
+    creator = CreateForests(**args)
     creator.create_forests()
 
+
 if __name__ == '__main__':
-  main()
+    main()
